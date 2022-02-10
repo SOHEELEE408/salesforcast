b94131e241280521ddf2af9cecb9ec8800b59204
minseok <minseok7407@naver.com>
2022-02-10 17:35:49 +0900

    Initial commit

---
+++ src/main/java/com/salesforcast/SalesforcastApplication.java
@@ -0,0 +1,13 @@
+package com.salesforcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesforcastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesforcastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salesforcast/SalesforcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesforcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesforcastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
